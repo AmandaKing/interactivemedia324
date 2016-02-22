a72038dff09a82ed4326200288df6d55f0de4c65
AmandaKing <aking13@wou.edu>
2016-02-21 16:29:29 -0800

    Project 2

---
+++ Project 2/links/assets/js/test.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.type').fadeOut(7000);
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+console.log("its working!");
+
+
---
+++ Project 2/links/assets2/js/test.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.type').fadeIn(7000);
+	$('.embarrassed').fadeIn(7000);
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+console.log("its working!");
+
+
